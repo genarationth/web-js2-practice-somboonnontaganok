211cbf518f23c7cc6e0e79df78e0aa9d849229dd
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-10 16:04:25 +0000

    finish my age in the future

---
+++ ageCalculation.js
@@ -0,0 +1,7 @@
+let myBirthYear = 1965;
+let myBirthMonth = 10;
+let futureYear = 2026;
+let futureMonth = 3;
+let myAge;
+futureMonth < myBirthMonth ? myAge = (futureYear - myBirthYear -1) : (myAge = myBirthYear - futureYear);
+console.log(`I am ${myAge} years old.`);
